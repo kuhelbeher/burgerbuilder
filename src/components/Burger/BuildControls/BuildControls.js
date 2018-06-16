dce8d04752cefb0a489b0a8b8200d86479e7484b
kuhelbeher <kuhelbeher@users.noreply.github.com>
2018-06-16 12:58:49 +0300

    Add buttons functionality

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,12 @@ const controls = [
 const buildControls = (props) => {
 	return (
 		<div className={classes.BuildControls}>
-			{controls.map(ctrl => <BuildControl key={ctrl.label} label={ctrl.label} />)}
+			{controls.map(ctrl => <BuildControl 
+				key={ctrl.label} 
+				label={ctrl.label}
+				added={() => props.ingredientAdded(ctrl.type)}
+				removed={() => props.ingredientRemoved(ctrl.type)}
+				disabled={props.disabled[ctrl.type]} />)}
 		</div>
 	);
 };
