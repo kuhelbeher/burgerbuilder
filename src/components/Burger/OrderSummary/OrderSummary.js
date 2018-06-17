d1268b5544a13af68563fff720067266a91bcdcc
kuhelbeher <kuhelbeher@users.noreply.github.com>
2018-06-17 13:43:15 +0300

    Some fixes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-	const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+	// This could be a functional component, it doesn't have to be a class component
+	// componentDidUpdate(prevProps, prevState)
+	// componentWillUpdate() {
+	// 	console.log('[OrderSummary] Will update');
+	// }
+
+	render () {
+		const ingredientSummary = Object.keys(this.props.ingredients)
 		.map(igKey => {
 			return (
 				<li key={igKey}>
-					<span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+					<span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
 				</li>
 			)
 		});
-	return (
-		<Fragment>
-			<h3>Your Order</h3>
-			<p>A delicious burger with the following ingredients:</p>
-			<ul>
-				{ingredientSummary}
-			</ul>
-			<p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-			<p>Continue to Checkout?</p>
-			<Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-			<Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-		</Fragment>
-	);
-};
-export default orderSummary;
+
+		return (
+			<Fragment>
+				<h3>Your Order</h3>
+				<p>A delicious burger with the following ingredients:</p>
+				<ul>
+					{ingredientSummary}
+				</ul>
+				<p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+				<p>Continue to Checkout?</p>
+				<Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+				<Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+			</Fragment>
+		);
+	};
+}
+
+export default OrderSummary;
