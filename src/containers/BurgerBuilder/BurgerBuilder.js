76f88a501d8a05de2d1494b3b41562d9e21149bf
kuhelbeher <kuhelbeher@users.noreply.github.com>
2018-06-20 17:02:38 +0300

    Added routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,31 +92,16 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseContinueHandler = () => {
-		// alert('YEEEa');
-		this.setState({loading: true});
-
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'Dmytro Bartieniev',
-				address: {
-					street: 'Street 1',
-					zipCode: '49000',
-					country: 'Ukraine'
-				},
-				email: 'test@test.ua'
-			},
-			deliveryMethod: 'fastest'
-		};
-
-		axios.post('/orders.json', order)
-			.then(response => {
-				this.setState({loading: false, purchasing: false});
-			})
-			.catch(error => {
-				this.setState({loading: false, purchasing: false});
-			});
+		const queryParams = [];
+		for (let i in this.state.ingredients) {
+			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+		}
+		queryParams.push(`price=${this.state.totalPrice}`);
+		const queryString = queryParams.join('&');
+		this.props.history.push({
+			pathname: '/checkout',
+			search: queryString
+		});
 	}
 
 	render() {
