bc00816df64911aa21bb4dbfe30df2d4781bce43
kuhelbeher <kuhelbeher@users.noreply.github.com>
2018-06-29 22:14:31 +0300

    Bug fixes

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -40,7 +40,7 @@ const removeIngredient = (state, action) => {
 
   const updatedState = {
     ingredients: updatedIngredients,
-    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
     building: true,
   };
 
